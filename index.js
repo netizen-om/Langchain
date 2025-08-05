76293bdc2ae12eb4e883c95becf9c3c17fe4fe82
netizen-om <borisagarom@gmail.com>
2025-08-05 20:14:59 +0530

    created few more tools

--- index.js
+++ index.js
@@ -11,3 +11,31 @@ const multiply = tool(async(({a, b}) => {
         b : z.number().describe("Second Number")
     })
 })
+
+const add = tool(
+  async ({ a, b }) => {
+    return a + b;
+  },
+  {
+    name: "add",
+    description: "Add two numbers together",
+    schema: z.object({
+      a: z.number().describe("first number"),
+      b: z.number().describe("second number"),
+    }),
+  }
+);
+
+const divide = tool(
+  async ({ a, b }) => {
+    return a / b;
+  },
+  {
+    name: "divide",
+    description: "Divide two numbers",
+    schema: z.object({
+      a: z.number().describe("first number"),
+      b: z.number().describe("second number"),
+    }),
+  }
+);
