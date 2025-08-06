5f281d98038be545df5331de5d1272e9ad707ac2
netizen-om <borisagarom@gmail.com>
2025-08-06 19:06:01 +0530

    create llm instance

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 import { tool } from "@langchain/core/tools";
 import z from "zod";
-import { GoogleGenerativeAI } from "@google/generative-ai";
+import { ChatGoogleGenerativeAI } from "@langchain/google-genai"
+
+const llm = new ChatGoogleGenerativeAI({
+  apiKey : process.env.GOOGLE_GEMINI_API_KEY,
+  model : "gemini-2.0-flash"
+})
 
 const multiply = tool(async(({a, b}) => {
     return a*b;
